3489b7367e585b1018571263dc976463aa37f565
ara2798 <angel.argandonavite@gmail.com>
2020-03-02 23:06:51 -0600

    Added main character and projectile-shooting mechanic

---
+++ Assets/Scripts/MCController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    Rigidbody2D rigidbody2d;
+    Vector2 lookDirection = new Vector2(1, 0);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        Vector2 position = rigidbody2d.position;
+        position = position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
---
+++ Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    private bool onClick;
+    public float rayLength = 5.0f;
+    Vector3 mouseVector;
+    Vector3 rayDirection;
+    LineRenderer aimRay;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimRay = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            onClick = true;
+            aimRay.enabled = true;
+        }
+        if (onClick)
+        {
+            aimRay.SetPosition(0, transform.position);
+            mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseVector.z = 0.0f;
+            rayDirection = transform.position - mouseVector;
+            rayDirection.Normalize();
+            rayDirection *= rayLength;
+            aimRay.SetPosition(1, transform.position + rayDirection);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            onClick = false;
+            aimRay.enabled = false;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Bullet bulletScript = bulletObject.GetComponent<Bullet>();
+        bulletScript.Launch(rayDirection, 200);
+    }
+
+}
